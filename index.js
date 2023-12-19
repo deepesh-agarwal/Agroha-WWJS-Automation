5369dd736f965c2e92e54a51436584c494793c1a
deepesh-agarwal <deepesh-agarwal@users.noreply.github.com>
2023-12-19 16:39:36 +0530

    Update index.js
    Bonfire

--- index.js
+++ index.js
@@ -29,15 +29,30 @@ app.get('/sendWelcome', (req, res) => {
 
 // Express server for handling '/sendWelcome' path
 app.get('/sendBonfire', (req, res) => {
-sendWelcome(name, phoneNumber)
-            .then(() => {
-                res.send(`Welcome message sent to ${name}`);
-            })
-            .catch(err => {
-                res.status(500).send(`Error sending message: ${err}`);
-            });
 
+        const { name, phoneNumber } = req.query;
+    
+    if (name && phoneNumber) {
+        sendBonfire(name, phoneNumber,'https://www.mosquitomagnet.com/media/Articles/Mosquito-Magnet/Dont-Fear-the-Fire.jpg');
+    } else {
+        res.status(400).send('Missing name or phoneNumber query parameters');
+    }
+    
 });
+
+async function sendBonfire(name, phoneNumber,imageUrl) {
+    const media = await MessageMedia.fromUrl('https://via.placeholder.com/350x150.png');
+        try {
+        // Create a MessageMedia instance from the URL
+        const media = await MessageMedia.fromUrl(imageUrl);
+
+        // Send the image to the specified number
+        client.sendMessage(`${phoneNumber}@c.us`, media, { caption: '`Hello ${name}, we offer Bonfire at dinner for just Rs.600/- !' });
+    } catch (error) {
+        console.error('Error sending image:', error);
+    }
+ 
+}
 
 async function sendWelcome(name, phoneNumber) {
     const message = `Dear ${name}, Thank you for your stay. Pls. Dial *'110'* for kitchen and *'9'* for Reception, you can also call *8881088844*. To access FREE WiFi service pls. connect to 'Hotel Agroha' with password - agroha123. We strictly prohibit any illegal activity in our premises like Gambling.`;
